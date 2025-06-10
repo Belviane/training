754af48f65467a666e40d607506d14cc76f48fa7
Belviane1 <songmenebelviane@gmal.com>
2025-06-10 12:35:06 +0100

    Modifications des seeders

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('utilisateurs')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         $this->call([
             RolesTableSeeder::class,
             UtilisateursTableSeeder::class,
--- backend/database/seeders/UtilisateursTableSeeder.php
+++ backend/database/seeders/UtilisateursTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 
 class UtilisateursTableSeeder extends Seeder
@@ -20,7 +21,7 @@ class UtilisateursTableSeeder extends Seeder
                 'genre' => 'Féminin',
                 'date_naissance' => '1988-11-15',
                 'login' => 'auditeur@gmail.com',
-                'mdp' => '$2y$12$XaXZ68g510DRzQHCCQv.bOi0iBqnEQIprNpY6MZyNOFWEYVup6ZuK',
+                'mdp' => Hash::make('auditeur123'),
                 'created_at' => now(),
                 'updated_at' => now(),
                 'is_active' => 1,
@@ -32,7 +33,7 @@ class UtilisateursTableSeeder extends Seeder
                 'genre' => 'Masculin',
                 'date_naissance' => '1982-08-05',
                 'login' => 'caissier@gmail.com',
-                'mdp' => '$2y$12$b/IPQFAnX/pVOvPkIVXRyOU0I9kCLmGwUwjmCqBJmh7i3OFc9UTQK',
+                'mdp' => Hash::make('caissier123'),
                 'created_at' => now(),
                 'updated_at' => now(),
                 'is_active' => 1,
@@ -44,7 +45,7 @@ class UtilisateursTableSeeder extends Seeder
                 'genre' => 'Féminin',
                 'date_naissance' => '1970-02-25',
                 'login' => 'parent@gmail.com',
-                'mdp' => '$2y$12$psOlB1YmVRZuusDw1dIAFe4CPDs.66J7Ai6mvFh8Xoz5fTclQ3pUS',
+                'mdp' => Hash::make('parent123'),
                 'created_at' => now(),
                 'updated_at' => now(),
                 'is_active' => 1,
@@ -56,7 +57,7 @@ class UtilisateursTableSeeder extends Seeder
                 'genre' => 'Féminin',
                 'date_naissance' => '1995-09-10',
                 'login' => 'apprenant@gmail.com',
-                'mdp' => '$2y$12$dZr4zZur.YzqABTzdA0CR.bS6kAqrzje29XU8ooEOo1DMgvRmyseW',
+                'mdp' => Hash::make('apprenant123'),
                 'created_at' => now(),
                 'updated_at' => now(),
                 'is_active' => 1,
@@ -68,7 +69,7 @@ class UtilisateursTableSeeder extends Seeder
                 'genre' => 'Masculin',
                 'date_naissance' => '1980-03-20',
                 'login' => 'formateur@gmail.com',
-                'mdp' => '$2y$12$Inc7pj/Px492T6N64KG.IO8DFwOkugtSdqeh4DH4WPpxfJU8nyNJa',
+                'mdp' => Hash::make('formateur123'),
                 'created_at' => now(),
                 'updated_at' => now(),
                 'is_active' => 1,
@@ -80,7 +81,7 @@ class UtilisateursTableSeeder extends Seeder
                 'genre' => 'Masculin',
                 'date_naissance' => '1985-06-15',
                 'login' => 'superviseur@gmail.com',
-                'mdp' => '$2y$12$GH9UDVLofqo5xIcqZ6mrFub357v8HfgZWLXFn2qDG72bAeGl69qK2',
+                'mdp' => Hash::make('superviseur123'),
                 'created_at' => now(),
                 'updated_at' => now(),
                 'is_active' => 1,
@@ -92,7 +93,7 @@ class UtilisateursTableSeeder extends Seeder
                 'genre' => 'Masculin',
                 'date_naissance' => '1990-01-01',
                 'login' => 'admin@gmail.com',
-                'mdp' => '$2y$12$LQChW.vnlVPPeespSIgrcOC6Xa/ofb2UQ4.5HXdC8EWTZc253F1U6',
+                'mdp' => Hash::make('admin123'),
                 'created_at' => now(),
                 'updated_at' => now(),
                 'is_active' => 1,
